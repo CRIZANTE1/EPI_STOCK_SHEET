1295703b62924017d1de65da6455f0291cde0845
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-04-14 11:26:13 -0300

    Update conection.py

--- API/conection.py
+++ API/conection.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pygsheets
 import os
+import json
 import logging
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -11,12 +12,15 @@ def connect_sheet():
         logging.info("Tentando conectar ao Google Sheets...")
 
         if "connections" in st.secrets and "gsheets" in st.secrets["connections"]:
-            # Carrega as credenciais do secrets.toml
+            # Converte o bloco do secrets em string JSON e armazena como variável de ambiente
             service_account_info = dict(st.secrets["connections"]["gsheets"])
             spreadsheet_url = service_account_info.pop("spreadsheet")
             
-            # Autoriza com pygsheets usando o dicionário
-            credentials = pygsheets.authorize(service_account_json=service_account_info)
+            json_credentials = json.dumps(service_account_info)
+            os.environ["GCP_SERVICE_ACCOUNT"] = json_credentials
+            
+            # Autorização via variável de ambiente
+            credentials = pygsheets.authorize(service_account_env_var="GCP_SERVICE_ACCOUNT")
         else:
             # Fallback local
             credentials_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'credentials', 'cred.json')
@@ -30,5 +34,4 @@ def connect_sheet():
         logging.error(f"Erro durante a autorização do Google Sheets: {e}")
         st.error(f"Erro durante a autorização do Google Sheets: {e}")
         return None, None
-
 
