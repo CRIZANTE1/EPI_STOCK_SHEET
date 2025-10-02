1281e11f9dcf831482f7e618a72b2c357112dece
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-10-02 10:51:57 -0300

    Update consulta_ca.py

--- pages/consulta_ca.py
+++ pages/consulta_ca.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Front.ca_lookup_page import ca_lookup_page
 
-def show():
+def page_consulta_ca():
     ca_lookup_page()
 
 if __name__ == "__main__":
-    show()
+    page_consulta_ca()
