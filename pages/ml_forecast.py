f215bca0a083292c847ef095421db929a7f713b2
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-10-02 11:02:07 -0300

    Create ml_forecast.py

---
+++ pages/ml_forecast.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from Front.ml_forecast_page import ml_forecast_page
+
+def page_ml_forecast():
+    """Rota para a página de previsão com ML"""
+    ml_forecast_page()
+
+if __name__ == "__main__":
+    page_ml_forecast()
